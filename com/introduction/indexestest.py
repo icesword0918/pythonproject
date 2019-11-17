285cf8b507134400e33efb14b63724f7d087a1f0
wzg <152119156@qq.com>
2019-11-17 23:26:09 +0800

    序列的索引学习

---
+++ com/introduction/indexestest.py
@@ -0,0 +1,20 @@
+verse = ["春眠不觉晓","python不得了","夜来爬数据","好评知多少"]
+verse1 = ["春眠不觉晓","python不得了","夜来爬数据","好评知多少"]
+print(verse[2])
+print(verse[-1])
+
+print(verse[0:2])
+print(verse[0:3:2])
+
+print(verse+verse1)
+
+print(verse*3)
+
+print("春眠不觉晓" in verse)
+
+print("春眠不觉晓" not in verse)
+
+print(len(verse+verse1))
+
+print(max(verse))
+print(min(verse))

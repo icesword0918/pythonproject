a6be133fa0158db5957b2f5cd7e369c4a523b75b
git <152119156@qq.com>
2019-11-26 17:26:34 +0800

    截取字符串学习

--- com/introduction/strcomopertest.py
+++ com/introduction/strcomopertest.py
@@ -10,3 +10,12 @@ str3='人生苦短，我用python!'
 print(len(str3))                # 不区分
 print(len(str3.encode()))       # 计算UTF-8编码的字符串的长度
 print(len(str3.encode('gbk')))  # 计算GBK编码的字符串长度
+
+# 截取字符串
+substr1=str3[1]
+substr2=str3[5:]
+substr3=str3[:5]
+substr4=str3[2:5]
+print(substr1+'\n'+substr2+'\n'+substr3+'\n'+substr4)
+
+# 分割、合并字符串

5455bafa88c232ccea62331a6fe33cfecf78e2d8
wzg <152119156@qq.com>
2019-12-05 21:18:46 +0800

    类的成员与访问限制学习

--- com/advanced/classusetest.py
+++ com/advanced/classusetest.py
@@ -64,3 +64,44 @@ print("一共有"+str(Geese.number)+"只大雁")
 
 Geese.beak="喙的基部较高，长度和头部的长度几乎相等"   #添加类属性
 print("第二只大雁的喙：",list1[1].beak)             #访问类属性
+
+# 实例属性
+class Geese:
+    """大雁类"""
+    def __init__(self):
+        self.neck="脖子较长"
+        self.wing="振翅频率高"
+        self.leg="腿位于身体的中心支点，行走自如"
+        print("我属于雁类，我有以下的特征：")
+        print(self.neck)
+        print(self.wing)
+        print(self.leg)
+
+geese=Geese()
+
+
+goose1=Geese()
+goose2=Geese()
+print()
+goose1.neck="脖子没有天鹅的长"
+print("goose1的neck属性：",goose1.neck)
+print("goose2的neck属性：",goose2.neck)
+
+
+# 访问限制
+class Swan:
+    """天鹅类"""
+    _neck_swan="天鹅的脖子很长"          # 定义保护属性
+    def __init__(self):
+        print("_init_():",Swan._neck_swan)   # 在实例方法中访问保护属性
+swan=Swan()                                  # 创建Swan类的实例
+print("直接访问：",swan._neck_swan)          # 保护属性可以通过实例名访问
+
+class Swan:
+    """天鹅类"""
+    __neck_swan="天鹅的脖子很长"          # 定义私有属性
+    def __init__(self):
+        print("_init_():",Swan.__neck_swan)   # 在实例方法中访问私有属性
+swan=Swan()                                  # 创建Swan类的实例
+print("加入类名：",swan._Swan__neck_swan)          # 私有属性，可以通过“实例名._类名__xxx”方式访问
+print("直接访问：",swan.__neck_swan)               # 私有属性不能通过实例名访问，出错

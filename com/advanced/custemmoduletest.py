944ea317b7f1b5d1ac093f3301e18e3710283365
wzg <152119156@qq.com>
2019-12-08 22:37:39 +0800

    自定义模块学习

---
+++ com/advanced/custemmoduletest.py
@@ -0,0 +1,9 @@
+# 使用import语句导入模块
+import functransmittest
+functransmittest.fun_bmi("SS",1.78,69)
+# print(functransmittest.fun_bmi("SS",1.78,69))
+
+# 使用from***import语句导入模块
+from functransmittest import fun_bmi,fun_bmi_upgrade
+
+
